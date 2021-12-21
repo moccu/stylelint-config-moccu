845a79b96d25287d18f2d9eb025cac3e28ccfbee
Sascha Künstler <sascha.kuenstler@moccu.com>
2021-12-21 14:07:23 +0100

    Add tailwindcss config

---
+++ tailwindcss/index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = {
+	extends: 'stylelint-config-sass-guidelines',
+	rules: {
+		'at-rule-no-unknown': [
+			true,
+			{
+				ignoreAtRules: [
+					'tailwind',
+					'apply',
+					'extends',
+					'layer',
+					'variants',
+					'responsive',
+					'screen'
+				]
+			}
+		],
+		'declaration-block-trailing-semicolon': null,
+		'no-descending-specificity': null,
+		'scss/at-rule-no-unknown': null
+	}
+}
+ 
